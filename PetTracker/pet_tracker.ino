84a3cc28c821ee7a50b4f9743d040256864e4573
Ridhima4870 <ridhima4870.se24@chitkara.edu.in>
2025-10-31 17:52:03 +0530

    Create pet_tracker.ino

---
+++ PetTracker/pet_tracker.ino
@@ -0,0 +1,13 @@
+// - Connect GPS to Arduino (TX, RX pins)
+// - Read location data
+// - Print coordinates via Serial for Raspberry Pi to read
+
+// Example idea 
+Start GPS connection
+Loop:
+   Read GPS latitude and longitude
+   If location is valid:
+       Print "Latitude, Longitude" to Serial
+   Wait 2 seconds
+
+  
